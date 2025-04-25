4c62f550cf39f631c2e67f6213779b1401bdb7e3
RedNova <petitkarol555@gmail.com>
2025-04-24 18:36:54 -0600

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1248,9 +1248,9 @@
         if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
           return false;
         }
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-          return false;
-        }
+        // if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        //   return false;
+        // }
         if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
           return false;
         }
